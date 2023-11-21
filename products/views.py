8ccae8e0abb63521d413fd0e5d7fc162064dc347
melikhovaeva <sshultse@vk.com>
2023-11-21 17:38:32 +0300

    Users FBV -> CBV + mixin Title

--- products/views.py
+++ products/views.py
@@ -7,30 +7,27 @@ from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 
 from products.models import ProductCategory, Product, Basket
+from common.views import TitleMixin
 from django.core.paginator import Paginator
 
 
-class IndexView(TemplateView): 
+class IndexView(TitleMixin, TemplateView):
   template_name = 'products/index.html'
+  title = 'Store'
   
-  def get_context_data(self, **kwargs):
-    context = super(IndexView, self).get_context_data()
-    context["title"] = 'Store'
-    return context
-  
-class ProductsListView(ListView):
+class ProductsListView(TitleMixin, ListView):
   model = Product
   template_name = 'products/products.html'
   paginate_by = 3
-  
+  title = 'Store - Каталог'
+
   def get_queryset(self):
     queryset = super(ProductsListView, self).get_queryset()
     category_id = self.kwargs.get('category_id')
-    return queryset.filter(category_id = category_id) if category_id else queryset
-  
+    return queryset.filter(category_id=category_id) if category_id else queryset
+        
   def get_context_data(self, *, object_list = None, **kwargs):
     context = super(ProductsListView, self).get_context_data()
-    context["title"] = 'Store - catalog'
     context["categories"] = ProductCategory.objects.all()
     return context
 
