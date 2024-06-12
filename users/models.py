a4c197299092d583288a2e4dc31c0b3c15c6237c
Melikhova Eva-Sofia <sshultse@vk.com>
2024-06-12 14:14:28 +0300

    add requirements.txt

--- users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-from simple_history.models import HistoricalRecords
-
-
-class User(AbstractUser):
-  image = models.ImageField(upload_to='users_images', null=True, blank=True)
-  history = HistoricalRecords()
