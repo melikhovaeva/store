8399702ffbf5d8be619bcc9ef3b422e757a47a9d
MacallanS <khametovartem@gmail.com>
2024-06-13 08:49:41 +0300

    Миша помоги

--- products/urls.py
+++ products/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
-from products.views import ProductsListView, basket_add, basket_remove
+from .views import ProductsListView, basket_add, basket_remove
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', ProductsListView.as_view(), name='index'),
-    path('category/<int:category_id>/', ProductsListView.as_view(), name='category'),
-    path('page/<int:page>/', ProductsListView.as_view(), name='paginator'),
+    path('', cache_page(60 * 2)(ProductsListView.as_view()), name='index'),
+    path('category/<int:category_id>/', cache_page(60 * 5)(ProductsListView.as_view()), name='category'),
+    path('page/<int:page>/', cache_page(60 * 2)(ProductsListView.as_view()), name='paginator'),
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
 ]
